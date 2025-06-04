e909a4e6caa5b9a3e01cea3a2a475d53fe994625
Aleksandra Kwiatkowska <126334346+kwiatkowskaa@users.noreply.github.com>
2025-06-04 22:19:15 +0200

    'correct sql scripts'

---
+++ generowanie_czasu.sql
@@ -0,0 +1,69 @@
+DECLARE @Year INT = 2024;
+
+DECLARE @StartDate DATE = CAST(CAST(@Year AS VARCHAR) + '-01-01' AS DATE);
+DECLARE @EndDate DATE = CAST(CAST(@Year AS VARCHAR) + '-12-31' AS DATE);
+
+;WITH DateSequence AS (
+    SELECT CAST(@StartDate AS DATETIME) AS DateValue
+    UNION ALL
+    SELECT DATEADD(MINUTE, 1, DateValue)
+    FROM DateSequence
+    WHERE DateValue < DATEADD(DAY, 1, @EndDate)
+)
+INSERT INTO DIM_CZAS (
+    CZAS_KEY,
+    DATE,
+    DATETIME,
+    HOUR,
+    MINUTE,
+    DAY_NUMBER,
+    DAY_SUFFIX,
+    WEEK_DAY_NUMBER,
+    WEEK_DAY_NAME,
+    WEEKEND_FLAG,
+    HOLIDAY_TEXT,
+    HOLIDAY_FLAG,
+    DAY_OF_YEAR_NUMBER,
+    WEEK_OF_MONTH_NUMBER,
+    WEEK_OF_YEAR_NUMBER,
+    ISO_WEEK_OF_YEAR_NUMBER,
+    MONTH_NUMBER,
+    MONTH_NAME,
+    QUARTER_NUMBER,
+    QUARTER_NAME,
+    YEAR_NUMBER
+)
+SELECT 
+    FORMAT(DateValue, 'ddMMyyyyHHmmss') AS CZAS_KEY,
+    CAST(DateValue AS DATE),
+    DateValue,
+    DATEPART(HOUR, DateValue),
+    DATEPART(MINUTE, DateValue),
+    DAY(DateValue),
+    CAST(DAY(DateValue) AS VARCHAR) + 
+        CASE 
+            WHEN DAY(DateValue) IN (11,12,13) THEN 'th'
+            WHEN DAY(DateValue) % 10 = 1 THEN 'st'
+            WHEN DAY(DateValue) % 10 = 2 THEN 'nd'
+            WHEN DAY(DateValue) % 10 = 3 THEN 'rd'
+            ELSE 'th'
+        END,
+    DATEPART(WEEKDAY, DateValue),
+    DATENAME(WEEKDAY, DateValue),
+    CASE WHEN DATEPART(WEEKDAY, DateValue) IN (1, 7) THEN 'YES' ELSE 'NO' END,
+    N'',
+    'NO',
+    DATEPART(DAYOFYEAR, DateValue),
+    (DAY(DateValue) - 1) / 7 + 1,
+    DATEPART(WEEK, DateValue),
+    DATEPART(ISO_WEEK, DateValue),
+    MONTH(DateValue),
+    DATENAME(MONTH, DateValue),
+    DATEPART(QUARTER, DateValue),
+    'Q' + CAST(DATEPART(QUARTER, DateValue) AS VARCHAR),
+    YEAR(DateValue)
+FROM DateSequence ds
+WHERE NOT EXISTS (
+    SELECT 1 FROM DIM_CZAS WHERE CZAS_KEY = FORMAT(ds.DateValue, 'ddMMyyyyHHmmss')
+)
+OPTION (MAXRECURSION 0);
